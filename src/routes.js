7397d14fdd41f35ea8f8ca94934503e4fa08ffc5
Salomao Souza <souza95salomao@gmail.com>
2021-03-26 14:01:04 -0400

    [UPDATE] Moving to EJS

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const routes = express.Router()
+
+const views = __dirname + '/views/'
+
+routes.get('/', (_req, res)=>res.render(views+'index'))
+routes.get('/photos', (_req, res)=>res.render(views+'fotos'))
+routes.get('/fale-conosco', (_req, res)=>res.render(views+'fale-conosco'))
+routes.get('/multimidia', (_req, res)=>res.render(views+'multimidia'))
+routes.get('/specs', (_req, res)=>res.render(views+'specs'))
+routes.get('/google-glass', (_req, res)=>res.sendFile(views+'google-glass.html'))
+
+module.exports = routes
