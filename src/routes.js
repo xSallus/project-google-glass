945f21eaa384986b7135f268642ef27fb8ff8bed
Salomao Souza <souza95salomao@gmail.com>
2021-03-26 16:04:57 -0400

    [UPDATE] Moving to EJS

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,6 @@ routes.get('/photos', (_req, res)=>res.render(views+'fotos'))
 routes.get('/fale-conosco', (_req, res)=>res.render(views+'fale-conosco'))
 routes.get('/multimidia', (_req, res)=>res.render(views+'multimidia'))
 routes.get('/specs', (_req, res)=>res.render(views+'specs'))
-routes.get('/google-glass', (_req, res)=>res.sendFile(views+'google-glass.html'))
+routes.get('/google-glass', (_req, res)=>res.render(views+'google-glass'))
 
 module.exports = routes
