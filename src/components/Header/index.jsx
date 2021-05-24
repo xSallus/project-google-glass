c4ed5f521fafccd60de568b869ee74dcfb059d49
xSallus <souza95salomao@gmail.com>
2021-05-24 13:17:56 -0400

    Refactoring to ReactJS

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from '../../styles/components/header.module.css'
+
+const Header = () => {
+    return (
+        <nav className={styles.header}></nav>
+    )
+}
+
+export default Header
