319ff67c8f5993ba511ed0e28fd14b12dbe9e6f0
xSallus <souza95salomao@gmail.com>
2021-05-27 13:31:01 -0400

    [UPDATE] First half of home page style concluded

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,26 +13,27 @@ const Home = () => {
     return (
         <div className={styles.homeContainer} >
             <main>
-                <SectionHeader title="know all about Google Glass" misc="Technologies &gt; Inovations" />
+                <SectionHeader title="Know all about Google Glass" misc="Technologies &gt; Inovations" />
                 <section>
-                    <span>What is it?</span>
+                    <h3>What is it?</h3>
                     <p>The <span>Google Glass</span> is a glass like accessory that makes possible for user to interact with a variety of contents in agumented reality. Also called Project Glass, this device is capable of take photos from voice commands, send instant messages and and make video-calls. It's launch is scheduled to 2014 and it's price can be up to US$ 1,5k. Actually <span>Google Glass</span> is at tests and has a full video recorded with the device. Besides that, the Search giant registered new patents anti-steal and screen unlock for the device.</p>
                     <img src="images/glass-quadro-homem-mulher.jpg" alt="Project Glass in action" />
+                    <small>How Google Glass look like</small>
                 </section>
                 <section>
-                    <span>Launch date</span>
+                    <h3>Launch date</h3>
                     <p>There is no official and specific date for the launch of this device. It should be available for demonstration from 2013 on, but it's launch should at least from 2014 on.</p>
                 </section>
                 <section>
-                    <span>Technical table for Goolge Glass</span>
+                    <h3>Technical table for Goolge Glass</h3>
                     <table></table>
                 </section>
                 <section>
-                    <span>How it works</span>
+                    <h3>How it works</h3>
                     <p>De acordo with fonts near Goolge, theese glasses will have na small LCD or AMOLED display on the top, to be in front of the user eyes. With the help of a cam and GPS, user can track itself, and so select option with head movement.</p>
                 </section>
                 <section>
-                    <span>What you can do with Google Glass?</span>
+                    <h3>What you can do with Google Glass?</h3>
                     <p>The marketing video from Google shows that you can make yourself a 'super-human', once the device can indicate how many meters your from your checkpoint, if the train station is open/closed, show climate, and even allows you to agend dates with just one voice command.</p>
                     <video 
                         height="120px"
