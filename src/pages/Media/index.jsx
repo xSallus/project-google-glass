39447a4210a8b8a3d6aa7225fccc2e4db00a587c
xSallus <souza95salomao@gmail.com>
2021-05-27 15:45:16 -0400

    [UPDATE] Media page style concluded

--- src/pages/Media/index.jsx
+++ src/pages/Media/index.jsx
@@ -5,15 +5,16 @@ import SeactionHeader  from '../../components/SectionHeader'
 import styles from '../../styles/pages/media.module.css'
 
 const Media = () => {
+
     return (
         <div className={styles.mediaContainer} >
             <SeactionHeader title="Video and sound" misc="Glass &gt; Multimedia" />
-            <img src="images/radio-tv.png" alt="Radio Tv background" />
+            <img src="/images/radio-tv.png" alt="Radio Tv background" />
             <video controls >
-                <source src="media/one-day.mp4" type="video/mp4" />
+                <source src="/media/one-day.mp4" type="video/mp4" />
             </video>
             <audio controls >
-                <source src="media/2009-lovers-carvings-bibio.mp3" type="audio/mp3" />
+                <source src="/media/2009-lovers-carvings-bibio.mp3" type="audio/mp3" />
             </audio>
         </div>
    )
