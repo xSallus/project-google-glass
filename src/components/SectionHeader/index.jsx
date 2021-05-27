8fa977061c5bb9fff07c4465c0ce59259abd99f1
xSallus <souza95salomao@gmail.com>
2021-05-27 14:13:59 -0400

    [UPDATE] SectionHeader style

--- src/components/SectionHeader/index.jsx
+++ src/components/SectionHeader/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import styles from '../../styles/components/section.header.module.css'
+
 const SectionHeader = ({ misc, title }) => {
   return (
-    <header>
+    <header className={styles.sectionHeader} >
       <p>{misc}</p>
       <h1>{title}</h1>
       <h2>by xSallus</h2>
-      <h3>Updated at Mar 26, 2021</h3>
+      <h3 style={{ background: 'rgba(0,0,0,0)' }} >Updated at Mar 26, 2021</h3>
     </header>
   );
 };
