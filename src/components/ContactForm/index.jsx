5a8b9e98d9f73306d3f82aa83ab9b68d72ce1604
xSallus <souza95salomao@gmail.com>
2021-05-27 09:56:25 -0400

    [UPDATE] Contact fForm inputs

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -2,48 +2,48 @@ import React from "react";
 
 const Form = () => {
   return (
-    <form>
+    <form method="#" id="" >
       <fieldset>
         <legend>User identity</legend>
         <div>
           <label>Name:</label>
-          <input type="text" placeholder="Your name" />
+          <input name="" id="" type="text" placeholder="Your name" />
         </div>
         <div>
           <label>Passphrase:</label>
-          <input type="password" />
+          <input name="" id="" type="password" />
         </div>
         <div>
           <label>Email:</label>
-          <input type="email" />
+          <input name="" id="" type="email" />
         </div>
         <fieldset>
           <legend>Gender</legend>
-          <radio value="male">Male</radio>
-          <radio value="female">Female</radio>
+          <radio name="" id="" value="male">Male</radio>
+          <radio name="" id="" value="female">Female</radio>
         </fieldset>
         <div>
           <label>Birthday:</label>
-          <input type="date" />
+          <input name="" id="" type="date" />
         </div>
       </fieldset>
       <fieldset>
         <legend>User address</legend>
         <div>
           <label>Logradouro:</label>
-          <input type="text" placeholder="St, Ave, Trav.." />
+          <input name="" id="" type="text" placeholder="St, Ave, Trav.." />
         </div>
         <div>
           <label>Number:</label>
-          <input type="number" />
+          <input name="" id="" type="number" />
         </div>
         <div>
           <label>State:</label>
-          <select>{/* options here*/}</select> 
+          <select name="" id="">{/* options here*/}</select> 
         </div>
         <div>
           <label>City:</label>
-          <input type="text" placeholder="Your city name" /> 
+          <input name="" id="" type="text" placeholder="Your city name" /> 
         </div>
       </fieldset>
       <fieldset>
@@ -51,14 +51,14 @@ const Form = () => {
         {/*Slider here*/}
         <div>
             <label>Message</label>
-            <textarea placeholder="Your message" />
+            <textarea placeholder="Your message" name="" id="" />
         </div>
       </fieldset>
       <fieldset>
         <legend>I wanna a Google Glass</legend>
         <div>
           <label>Color</label>
-          <select>
+          <select name="" id="" >
             <option value="#f00" >Red</option>
             <option value="#00f" >Blue</option>
             <option value="#0f0" >Green</option>
