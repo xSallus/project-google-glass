5bca37b1238b5e03a4bfee2b0558fcf0a62599cf
xSallus <souza95salomao@gmail.com>
2021-05-27 11:44:56 -0400

    [ADD] Photos page content

--- src/pages/Photos/index.jsx
+++ src/pages/Photos/index.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from '../../styles/pages/photos.module.css'
 
 const Photos = () => {
+    const [photo, setPhoto] = useState('images/galeria-01.jpg')
+
     return (
         <div className={styles.photosContainer} >
-            <h1>Photos page</h1>
+            <img src={photo} alt="Selection" />
+            <div>
+                <img
+                  src="images/galeria-01.jpg"
+                  onCLick={()=>setPhoto('images/galeria-01.jpg')}
+                  alt="Option 01" />
+                <img
+                  src="images/galeria-02.jpg"
+                  onCLick={()=>setPhoto('images/galeria-02.jpg')}
+                  alt="Option 02" />
+                <img
+                  src="images/galeria-03.jpg"
+                  onCLick={()=>setPhoto('images/galeria-03.jpg')}
+                  alt="Option 03" />
+                <img
+                  src="images/galeria-04.jpg"
+                  onCLick={()=>setPhoto('images/galeria-04.jpg')}
+                  alt="Option 04" />
+                <img
+                  src="images/galeria-05.jpg"
+                  onCLick={()=>setPhoto('images/galeria-05.jpg')}
+                  alt="Option 05" />
+                <img
+                  src="images/galeria-06.jpg"
+                  onCLick={()=>setPhoto('images/galeria-06.jpg')}
+                  alt="Option 06" />
+            </div>
         </div>
    )
 }
