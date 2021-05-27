c3eccefae560288116fc0245d0d2c8e4ca410218
xSallus <souza95salomao@gmail.com>
2021-05-27 12:37:10 -0400

    [UPDATE] Specs page contents

--- src/pages/Specs/index.jsx
+++ src/pages/Specs/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
+import SectionHeader from '../../components/SectionHeader'
+
 import styles from '../../styles/pages/specs.module.css'
 
 const Specs = () => {
+    
     return (
         <div className={styles.specsContainer} >
-            <h1>Specs page</h1>
+            <SectionHeader title="X-ray on Google Glass" misc="Glass &gt; Specs" />
+            <div>
+                <p>Click on any area of the image below to see more info about the resourses of Google Glass</p>
+                <p>Any red point gets to a place full of information</p>
+                <div>
+                    <section>
+                        <span>Click on any detatched area in red</span>
+                        <img src="images/mao.png" alt="Apointing hand icon" />
+                    </section>
+                </div>
+            </div>
         </div>
     )
 }
