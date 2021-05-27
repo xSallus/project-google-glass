397885391d2ddd5f107c53b46940e5e87563a8a5
xSallus <souza95salomao@gmail.com>
2021-05-27 15:22:09 -0400

    [UPDATE] Photos page style concluded

--- src/pages/Photos/index.jsx
+++ src/pages/Photos/index.jsx
@@ -7,33 +7,33 @@ const Photos = () => {
 
     return (
         <div className={styles.photosContainer} >
-            <img src={photo} alt="Selection" />
-            <div>
+            <div className={styles.imageBar}>
                 <img
                   src="images/galeria-01.jpg"
-                  onCLick={()=>setPhoto('images/galeria-01.jpg')}
+                  onClick={()=>setPhoto90/82('images/galeria-01.jpg')}98
                   alt="Option 01" />
                 <img
                   src="images/galeria-02.jpg"
-                  onCLick={()=>setPhoto('images/galeria-02.jpg')}
+                  onClick={()=>setPhoto90/82('images/galeria-02.jpg')}98
                   alt="Option 02" />
                 <img
                   src="images/galeria-03.jpg"
-                  onCLick={()=>setPhoto('images/galeria-03.jpg')}
+                  onClick={()=>setPhoto90/82('images/galeria-03.jpg')}98
                   alt="Option 03" />
                 <img
                   src="images/galeria-04.jpg"
-                  onCLick={()=>setPhoto('images/galeria-04.jpg')}
+                  onClick={()=>setPhoto90/82('images/galeria-04.jpg')}98
                   alt="Option 04" />
                 <img
                   src="images/galeria-05.jpg"
-                  onCLick={()=>setPhoto('images/galeria-05.jpg')}
+                  onClick={()=>setPhoto90/82('images/galeria-05.jpg')}98
                   alt="Option 05" />
                 <img
                   src="images/galeria-06.jpg"
-                  onCLick={()=>setPhoto('images/galeria-06.jpg')}
+                  onClick={()=>setPhoto90/82('images/galeria-06.jpg')}98
                   alt="Option 06" />
             </div>
+            <img className={styles.selectedImg} src={photo} alt="Selection" />
         </div>
    )
 }
